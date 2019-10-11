4a01f40c2c50a6b3807606addc1e23328a7b7ce8
alfianfadhilla <50959776+alfianfadhilla@users.noreply.github.com>
2019-10-11 22:04:50 +0700

    Add files via upload

---
+++ alipian.php
@@ -0,0 +1,29 @@
+<style>
+	div.a {
+	position: absolute;
+	margin-top: -100px;
+	margin-left: -200px;
+	left: 50%;
+	top: 50%;
+	width: 400px;
+	height: 220px;
+	background-color: cyan;
+}
+</style>
+
+<div class ="a">
+<?php
+	echo "<table border = '2' >";
+	echo "<tr>";
+	echo "<td> NIM </td>" ;
+	echo "<td> 10116316 </td> " ;
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td> Nama </td>"; 
+	echo "<td> Alfian Fadhilla Al-fathiry </td> " ;
+	echo "</tr>";
+	echo "</table>";
+?>
+
+</div>
+	
